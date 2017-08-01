655eaa9d81abfa2a033932ff20ba2105048c2e85
Daniel Geurts <danielg13@live.com.au>
2017-08-01 18:51:09 +1000

    Corrected location permission request, sorted code in current weather fragment.

--- app/src/main/java/com/danielg/weatherapp/WeatherCurrentFragment.java
+++ app/src/main/java/com/danielg/weatherapp/WeatherCurrentFragment.java
@@ -2,6 +2,7 @@ package com.danielg.weatherapp;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -13,7 +14,6 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -30,11 +30,16 @@ import android.widget.Toast;
 
 import com.danielg.weatherapp.data.WeatherData;
 import com.danielg.weatherapp.data.WeatherService;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
 import com.google.gson.Gson;
 
 import java.util.Locale;
@@ -50,7 +55,8 @@ public class WeatherCurrentFragment extends Fragment {
 
     private static final String TAG = WeatherCurrentFragment.class.getSimpleName();
 
-    private static final int PERMISSION_REQUEST_LOCATION = 10;
+    private static final int REQUEST_PERMISSION_LOCATION = 10;
+    private static final int REQUEST_CHECK_SETTINGS = 11;
     private static final int UPDATE_INTERVAL_TIME = 10000;
     private static final int UPDATE_INTERVAL_FAST_TIME = UPDATE_INTERVAL_TIME / 2;
 
@@ -65,6 +71,8 @@ public class WeatherCurrentFragment extends Fragment {
     private LocationCallback mLocationCallback;
     private LocationRequest mLocationRequest;
     private Location mCurrentLocation;
+    private SettingsClient mSettingsClient;
+    private LocationSettingsRequest mLocationSettingsRequest;
     private WeatherData mWeatherData;
 
     private ImageView imageWeatherIcon;
@@ -84,33 +92,12 @@ public class WeatherCurrentFragment extends Fragment {
         loadPreferences(mContext);
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(mContext);
+        mSettingsClient = LocationServices.getSettingsClient(mContext);
+
         createLocationCallback();
         createLocationRequest();
-
+        buildLocationSettingsRequest();
         setWeatherValues();
-    }
-
-    private void loadPreferences(Context context) {
-        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
-        mAutoUpdateLocation = pref.getBoolean(AppSettingsFragment.KEY_GET_AUTO_LOCATION, true);
-        if (!mAutoUpdateLocation) {
-            String json = pref.getString(AppSettingsFragment.KEY_CURRENT_MANUAL_LOCATION, "");
-            mCurrentLocation = new Gson().fromJson(json, Location.class);
-        }
-        String unit = pref.getString(AppSettingsFragment.KEY_UNITS, "");
-        switch (unit) {
-            case "Metric (Default)":
-                mUnits = "si";
-                break;
-            case "Imperial":
-                mUnits = "us";
-                break;
-            default:
-                Log.d(TAG, "Could not get unit preference for some reason, defaulting to Metric (SI)");
-                mUnits = "si";
-                break;
-        }
-        mAPIKey = pref.getString(AppSettingsFragment.KEY_API_KEY, "");
     }
 
     @Override
@@ -162,24 +149,6 @@ public class WeatherCurrentFragment extends Fragment {
         setWeatherValues();
     }
 
-    // TODO Work something out here
-    private void updateValuesFromBundle(Bundle savedInstanceState) {
-        if (savedInstanceState != null) {
-            if (savedInstanceState.keySet().contains(KEY_LOCATION)) {
-                mCurrentLocation = savedInstanceState.getParcelable(KEY_LOCATION);
-            }
-            if (savedInstanceState.keySet().contains(KEY_WEATHER_DATA)) {
-                mWeatherData = (WeatherData) savedInstanceState.getSerializable(KEY_WEATHER_DATA);
-            }
-            setWeatherValues();
-        }
-    }
-
-    private boolean checkPermissions() {
-        int permissionState = ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION);
-        return permissionState == PackageManager.PERMISSION_GRANTED;
-    }
-
     @Override
     public void onPause() {
         super.onPause();
@@ -187,32 +156,18 @@ public class WeatherCurrentFragment extends Fragment {
         saveWeatherData();
     }
 
-    private void saveWeatherData() {
-        SharedPreferences pref = getActivity().getPreferences(Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = pref.edit();
-        Gson gson = new Gson();
-        String location = gson.toJson(mWeatherData);
-        editor.putString(KEY_LOCATION, location);
-        editor.commit();
-    }
-
-    // TODO And here...
-    /*@Override
-    public void onSaveInstanceState(Bundle outState) {
-        outState.putParcelable(KEY_LOCATION, mCurrentLocation);
-        super.onSaveInstanceState(outState);
-    }*/
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.i(TAG, "onRequestPermissionsResult");
         switch (requestCode) {
-            case PERMISSION_REQUEST_LOCATION: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.d(TAG, "Permission has been granted, will now search for location");
+            case REQUEST_PERMISSION_LOCATION: {
+                if (grantResults.length <= 0) {
+                    Log.i(TAG, "User cancelled permissions");
+                } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i(TAG, "Permission has been granted, will now search for location");
                     startLocationUpdates();
                 } else {
-                    Log.d(TAG, "Permission has been denied, user will have to enter location manually");
+                    Log.i(TAG, "Permission has been denied, user will have to enter location manually");
                     Snackbar.make(getActivity().findViewById(R.id.coordinator_layout),
                             "Location permission denied.",
                             Snackbar.LENGTH_SHORT).show();
@@ -237,6 +192,65 @@ public class WeatherCurrentFragment extends Fragment {
         return super.onOptionsItemSelected(item);
     }
 
+    // TODO Work something out here
+    private void updateValuesFromBundle(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            if (savedInstanceState.keySet().contains(KEY_LOCATION)) {
+                mCurrentLocation = savedInstanceState.getParcelable(KEY_LOCATION);
+            }
+            if (savedInstanceState.keySet().contains(KEY_WEATHER_DATA)) {
+                mWeatherData = (WeatherData) savedInstanceState.getSerializable(KEY_WEATHER_DATA);
+            }
+            setWeatherValues();
+        }
+    }
+
+    // TODO And here...
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        // outState.putParcelable(KEY_LOCATION, mCurrentLocation);
+        // super.onSaveInstanceState(outState);
+    }
+
+    private void saveWeatherData() {
+        SharedPreferences pref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        Gson gson = new Gson();
+        String location = gson.toJson(mWeatherData);
+        editor.putString(KEY_LOCATION, location);
+        editor.commit();
+    }
+
+    private void loadPreferences(Context context) {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
+        mAutoUpdateLocation = pref.getBoolean(AppSettingsFragment.KEY_GET_AUTO_LOCATION, true);
+        if (!mAutoUpdateLocation) {
+            String json = pref.getString(AppSettingsFragment.KEY_CURRENT_MANUAL_LOCATION, "");
+            mCurrentLocation = new Gson().fromJson(json, Location.class);
+        }
+        String unit = pref.getString(AppSettingsFragment.KEY_UNITS, "");
+        switch (unit) {
+            case "Metric (Default)":
+                mUnits = "si";
+                break;
+            case "Imperial":
+                mUnits = "us";
+                break;
+            default:
+                Log.d(TAG, "Could not get unit preference for some reason, defaulting to Metric (SI)");
+                mUnits = "si";
+                break;
+        }
+        mAPIKey = pref.getString(AppSettingsFragment.KEY_API_KEY, "");
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL_TIME);
+        mLocationRequest.setFastestInterval(UPDATE_INTERVAL_FAST_TIME);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
     private void createLocationCallback() {
         mLocationCallback = new LocationCallback() {
             @Override
@@ -249,15 +263,37 @@ public class WeatherCurrentFragment extends Fragment {
         };
     }
 
-    protected void createLocationRequest() {
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(UPDATE_INTERVAL_TIME);
-        mLocationRequest.setFastestInterval(UPDATE_INTERVAL_FAST_TIME);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    private void buildLocationSettingsRequest() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        mLocationSettingsRequest = builder.build();
     }
 
     private void startLocationUpdates() {
-        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+        mSettingsClient.checkLocationSettings(mLocationSettingsRequest)
+                .addOnSuccessListener(getActivity(), locationSettingsResponse -> {
+                    Log.i(TAG, "All location settings are satisfied");
+                    mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+                })
+                .addOnFailureListener(getActivity(), e -> {
+                    int statusCode = ((ApiException) e).getStatusCode();
+                    switch (statusCode) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                            Log.i(TAG, "Location settings not satisfied, attempting to upgrade settings.");
+                            try {
+                                ResolvableApiException rae = (ResolvableApiException) e;
+                                rae.startResolutionForResult(getActivity(), REQUEST_CHECK_SETTINGS);
+                            } catch (IntentSender.SendIntentException sie) {
+                                Log.i(TAG, "PendingIntent unable to execute result.");
+                            }
+                            break;
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            String errorMessage = "Location settings are inadequate and cannot be fixed here, change in settings.";
+                            Log.e(TAG, errorMessage);
+                            Toast.makeText(mContext, errorMessage, Toast.LENGTH_SHORT).show();
+                            mAutoUpdateLocation = false;
+                    }
+                });
     }
 
     private void pauseLocationUpdates() {
@@ -267,10 +303,23 @@ public class WeatherCurrentFragment extends Fragment {
         mFusedLocationClient.removeLocationUpdates(mLocationCallback);
     }
 
+    private boolean checkPermissions() {
+        int permissionState = ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
     private void requestLocationPermission() {
-        if (ContextCompat.checkSelfPermission(mContext,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Display rationale explaining why app needs permission
+            Log.d(TAG, "Displaying permission rationale to user");
+            Snackbar.make(getActivity().findViewById(R.id.coordinator_layout),
+                    "Location is needed for core functionality", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("OK", view -> {
+                        // Request the permission
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                REQUEST_PERMISSION_LOCATION);
+                    }).show();
+        } else {
             // Display snack bar detailing to user why location permission is needed
             // Otherwise, they can enter the location manually
             Log.d(TAG, "Prompting user permission for location");
@@ -279,24 +328,14 @@ public class WeatherCurrentFragment extends Fragment {
                     Snackbar.LENGTH_INDEFINITE).setAction("OK", view -> {
                 // Request the permission
                 requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        PERMISSION_REQUEST_LOCATION);
+                        REQUEST_PERMISSION_LOCATION);
             }).show();
-        } else {
-            // For older versions of Android (5.1 and lower)?
-            Log.d(TAG, "Prompting user permission for location");
-            Snackbar.make(getActivity().findViewById(R.id.coordinator_layout),
-                    "Permissions prompt not available. Requesting location permission.",
-                    Snackbar.LENGTH_SHORT).show();
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSION_REQUEST_LOCATION);
         }
     }
 
     private void refreshWeather() {
         if (mCurrentLocation != null && mAPIKey != null) {
             if (mAPIKey.length() > 0) {
-
-
                 Log.d(TAG, "Weather updating");
 
                 showProgress();
